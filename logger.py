0f737d8026e3c0eccf6a614e8a87f7165623dc0f
_Ghost_ <56601374+duch3201@users.noreply.github.com>
2023-01-14 22:18:56 +0100

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from datetime import date
+import uuid
+
+#made by duch3201 on github, this lib does not come with any warrenty,
+#if you wish to modify this code and re distribute it please give credit to og author https://github.com/duch3201
+id = uuid.uuid4()
+name = str(id)[:4]
+
+try:
+    os.mkdir("logs")
+except FileExistsError:
+    pass
+
+os.chdir("logs")
+
+def info(message):
+    mes = "[I] " + str(date.today()) + " | " + message
+    with open(name+".log", 'a') as logfile:
+        logfile.write(mes + "\n")
+
+def warning(message):
+    mes = "[W] " + str(date.today()) + " | " + message
+    with open(name+".log", 'a') as logfile:
+        logfile.write(mes + "\n")
+
+def error(message):
+    mes = "[E] " + str(date.today()) + " | " + message
+    with open(name+".log", 'a') as logfile:
+        logfile.write(mes + "\n")
+
+def debug(message):
+    mes = "[D] " + str(date.today()) + " | " + message
+    with open(name+".log", 'a') as logfile:
+        logfile.write(mes + "\n")
